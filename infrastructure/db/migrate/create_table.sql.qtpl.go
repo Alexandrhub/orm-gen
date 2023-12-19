a980a9c54eabb5cc5afa434f652ee3d1e7f269a4
Alexandr <alexandrhub@vk.com>
2023-12-19 13:50:47 +0300

    fix: goreleaser

--- infrastructure/db/migrate/create_table.sql.qtpl.go
+++ infrastructure/db/migrate/create_table.sql.qtpl.go
@@ -8,10 +8,9 @@ package migrate
 import (
 	"github.com/Alexandrhub/cli-orm-gen/infrastructure/db/scanner"
 	"github.com/Alexandrhub/cli-orm-gen/utils"
-)
 
 //line create_table.sql.qtpl:6
-import (
+
 	qtio422016 "io"
 
 	qt422016 "github.com/valyala/quicktemplate"
